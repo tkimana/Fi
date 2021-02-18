b9b36ff5838e0b443096196b8440de11b4b1996f
tkimana <kimana.theodore@gmail.com>
2021-02-18 13:59:51 -0600

    added some changes

--- SoftwareEngineer.py
+++ SoftwareEngineer.py
@@ -9,7 +9,6 @@ def code(se):
 
 code(se2)
 
-
 class SoftwareEngineer:
     # Class attribute
     alies= 'Magic keyboard'
@@ -20,8 +19,7 @@ class SoftwareEngineer:
         self.age= age
         self.level= level
         self.salary= salary
-        self.active=True
-
+        #self.active=True
 
 
 # Instance Method
@@ -32,10 +30,25 @@ class SoftwareEngineer:
 
         print(f'{self.name} is coding in {language}' )
 
-    def information(self):
-        information= f'name: {self.name}, age: {self.age}, {self.level}, {self.active}'
+    #def information(self):
+        #information= f'name: {self.name}, age: {self.age}, {self.level}, {self.active}'
+        #return information
+    # Dunder Method
+    def __str__(self):
+        information= f'name: {self.name}, age: {self.age}, {self.level}'
         return information
-    
+
+    def __eq__(self, other):
+        return self.name == other.name and self.age == other.age
+
+    @staticmethod
+    def entry_salary(level):
+        if level == 'Junior':
+            return 5000
+        if level == 'Sunior':
+            return 4000
+        return 100
+
 
 
     
@@ -43,13 +56,14 @@ class SoftwareEngineer:
 
 
 # Instance 
-Se1= SoftwareEngineer('greg', 23, 'Entry Level', '80k')
+Se1= SoftwareEngineer('greg', 23, 'Entry Level', '80k', 'Junior')
+Se2= SoftwareEngineer('greg', 23, 'Entry Level', '80k', 'Sunior')
 print(Se1.name, Se1.age, Se1.level, Se1.salary)
 print(Se1.alies)
 Se1.code()
 Se1.code_in_language('Java')
-
-print(Se1.information())
+print(Se1==Se2)
+print(Se1.entry_salary)
 
 # Recap
 # Create a class(blueprint)
