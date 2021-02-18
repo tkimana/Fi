6aa7e6907b208fd20ef5548f0e1cdeadca6c4b0a
tkimana <kimana.theodore@gmail.com>
2021-02-18 12:39:58 -0600

    added some changes

--- SoftwareEngineer.py
+++ SoftwareEngineer.py
@@ -2,6 +2,8 @@
 se1=["Software Engineer", "Joe", 10, "junior", 3000]
 se2=["Software Engineer", "Alex", 100, "Sunior", 5000]
 
+
+# Creating a Function in Python
 def code(se):
     print(f'{se[1]} is writing code ...')
 
@@ -18,7 +20,25 @@ class SoftwareEngineer:
         self.age= age
         self.level= level
         self.salary= salary
+        self.active=True
 
+
+
+# Instance Method
+    def code(self):
+        print(f'{self.name} is writing code ...')
+
+    def code_in_language(self, language):
+
+        print(f'{self.name} is coding in {language}' )
+
+    def information(self):
+        information= f'name: {self.name}, age: {self.age}, {self.level}, {self.active}'
+        return information
+    
+
+
+    
 
 
 
@@ -26,6 +46,10 @@ class SoftwareEngineer:
 Se1= SoftwareEngineer('greg', 23, 'Entry Level', '80k')
 print(Se1.name, Se1.age, Se1.level, Se1.salary)
 print(Se1.alies)
+Se1.code()
+Se1.code_in_language('Java')
+
+print(Se1.information())
 
 # Recap
 # Create a class(blueprint)
